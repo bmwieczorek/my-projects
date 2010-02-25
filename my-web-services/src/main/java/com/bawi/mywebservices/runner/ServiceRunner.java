3aed9e3445a3a434a1f8706fb3c8c39873c267e8
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-02-25 17:23:59 +0000

    service working

--- my-web-services/src/main/java/com/bawi/mywebservices/runner/ServiceRunner.java
+++ my-web-services/src/main/java/com/bawi/mywebservices/runner/ServiceRunner.java
@@ -1,22 +1,47 @@
 package com.bawi.mywebservices.runner;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.TreeMap;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ServiceRunner {
 
 	public ServiceRunner() {
-		String[] configLocations = new String[] {"cxf-context.xml", "services-context.xml"};
+		String[] configLocations = new String[] { "cxf-context.xml",
+				"services-context.xml" };
 		new ClassPathXmlApplicationContext(configLocations);
+		loadProperties();
+
 	}
-	
+
 	public static void main(String[] args) {
 		new ServiceRunner();
-		while(true){
+		while (true) {
 			try {
 				Thread.sleep(Long.MAX_VALUE);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private void loadProperties() {
+		InputStream stream = ClassLoader
+				.getSystemResourceAsStream("service-runner.properties");
+		Properties properties = new Properties();
+		try {
+			properties.load(stream);
+			System.getProperties().putAll(properties);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		TreeMap treeMap = new TreeMap(System.getProperties());
+		for (Object key : treeMap.keySet()) {
+			System.out.println(key + "=" + treeMap.get(key));
 		}
 	}
 }
