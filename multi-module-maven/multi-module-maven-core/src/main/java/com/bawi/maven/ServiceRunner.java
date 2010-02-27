673f2b93230ae597532a5936ba8be7266e1b2091
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-02-27 09:22:05 +0000

    skeleton

---
+++ multi-module-maven/multi-module-maven-core/src/main/java/com/bawi/maven/ServiceRunner.java
@@ -0,0 +1,41 @@
+package com.bawi.maven;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.TreeMap;
+
+public class ServiceRunner {
+
+	public ServiceRunner() {
+		loadProperties();
+	}
+
+	public static void main(String[] args) {
+		new ServiceRunner();
+		while (true) {
+			try {
+				Thread.sleep(Long.MAX_VALUE);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static void loadProperties() {
+		InputStream stream = ClassLoader
+				.getSystemResourceAsStream("service-runner.properties");
+		Properties properties = new Properties();
+		try {
+			properties.load(stream);
+			System.getProperties().putAll(properties);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		TreeMap treeMap = new TreeMap(System.getProperties());
+		for (Object key : treeMap.keySet()) {
+			System.out.println(key + "=" + treeMap.get(key));
+		}
+	}
+}
