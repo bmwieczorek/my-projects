91a3ec14c655528e2c450ad6b6b90e477563fd06
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-02-27 10:28:06 +0000

    added comments how to run

--- multi-module-maven/multi-module-maven-war/src/main/java/com/bawi/maven/ServiceRunner.java
+++ multi-module-maven/multi-module-maven-war/src/main/java/com/bawi/maven/ServiceRunner.java
@@ -1,6 +1,5 @@
 package com.bawi.maven;
 
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -8,14 +7,17 @@ import java.util.TreeMap;
 
 public class ServiceRunner {
 
+	// run via: export M2_REPO=~/dev/environment/apache-maven-2.0.9/localRepo; java -classpath
+	// $M2_REPO/com/bawi/multi-module-maven-war/0.0.1-SNAPSHOT/multi-module-maven-war-0.0.1-SNAPSHOT.jar:$M2_REPO/com/bawi/multi-module-maven-core/0.0.1-SNAPSHOT/multi-module-maven-core-0.0.1-SNAPSHOT.jar
+	// com.bawi.maven.ServiceRunner
+
 	public static void main(String[] args) {
 		loadProperties();
 	}
 
 	@SuppressWarnings("unchecked")
 	private static void loadProperties() {
-		InputStream stream = ClassLoader
-				.getSystemResourceAsStream("service-runner.properties");
+		InputStream stream = ClassLoader.getSystemResourceAsStream("service-runner.properties");
 		Properties properties = new Properties();
 		try {
 			properties.load(stream);
@@ -27,5 +29,6 @@ public class ServiceRunner {
 		for (Object key : treeMap.keySet()) {
 			System.out.println(key + "=" + treeMap.get(key));
 		}
+		System.out.println("War service runner");
 	}
 }
