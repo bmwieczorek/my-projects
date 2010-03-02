4bf5c2e460620d33637551d60950b6c28094e3ea
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-03-02 17:39:48 +0000

    added log4j and spring support

---
+++ webapp-log4j-spring/src/main/java/com/bawi/mywebapp/About.java
@@ -0,0 +1,30 @@
+package com.bawi.mywebapp;
+
+import java.util.TreeMap;
+
+import org.apache.log4j.Logger;
+
+public class About {
+	private static final Logger logger = Logger.getLogger(About.class);
+	
+	public About() {
+		displaySystemProperties();
+		try {
+			throw new AboutException("My exception!!!");
+		} catch (AboutException e) {
+			logger.error("Throwing my exception !!!");
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void displaySystemProperties() {
+		TreeMap treeMap = new TreeMap(System.getProperties());
+		for (Object key : treeMap.keySet()) {
+			logger.debug(key + "=" + treeMap.get(key));
+		}
+	}
+	
+	public void destroy(){
+		logger.error("Destroying ...");
+	}
+}
---
+++ webapp-log4j-spring/src/main/java/com/bawi/mywebapp/AboutException.java
@@ -0,0 +1,13 @@
+package com.bawi.mywebapp;
+
+public class AboutException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public AboutException() {
+	}
+	
+	public AboutException(String message) {
+		super(message);
+	}
+}
