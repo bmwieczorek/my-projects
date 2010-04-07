d644d1b5b4decc254bb12ade232b0be69d070aa0
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-04-07 11:54:38 +0000

    example

---
+++ my-java/src/main/java/autoboxing/Example.java
@@ -0,0 +1,12 @@
+package autoboxing;
+
+public class Example {
+	public static void main(String[] args) {
+		int i = returnInteger();
+		System.out.println(i);
+	}
+
+	private static Integer returnInteger() {
+		return null;
+	}
+}
