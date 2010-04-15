042936565515f1168a701e641139bef6485654a5
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-04-15 18:54:10 +0000

    clean up

--- my-java/src/main/java/serialization/TestApplet.java
+++ my-java/src/main/java/serialization/TestApplet.java
@@ -4,7 +4,10 @@ import javax.swing.JApplet;
 import javax.swing.JButton;
 
 public class TestApplet extends JApplet{
-    public void init(){
+
+	private static final long serialVersionUID = 1L;
+
+	public void init() {
         JButton b1 = new JButton("one");
         JButton b2 = new JButton("two");
         JButton b3 = new JButton("three");
