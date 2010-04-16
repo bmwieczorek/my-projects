e68826cb08481f1231d9778ed4b36bc317252a4a
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-04-16 12:47:16 +0000

    clean up

--- my-java/src/main/java/CalendarVsDate.java
+++ my-java/src/main/java/CalendarVsDate.java
@@ -1,33 +1,17 @@
 import java.util.Calendar;
 import java.util.Date;
 
-
 public class CalendarVsDate {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        Date d = new Date();
+	public static void main(String[] args) {
+		Date d = new Date();
+		System.out.println(d.getTime());
 
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        
-        Calendar c = Calendar.getInstance();
-        //c.add(Calendar.DATE, 5);
-        
-        System.out.println(d.getTime());
-        //System.out.println(c.getTimeInMillis());
-        
-        c.setTimeInMillis(d.getTime());
-        
-        System.out.println(c.getTimeInMillis());
+		Calendar c = Calendar.getInstance();
+		// c.add(Calendar.DATE, 5);
 
-    }
+		c.setTimeInMillis(d.getTime());
 
+		System.out.println(c.getTimeInMillis());
+	}
 }
