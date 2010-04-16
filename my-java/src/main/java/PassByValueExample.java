693ef1b79739b848e26e6c530d8b517ed9ab4709
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-04-16 12:33:51 +0000

    clean up

---
+++ my-java/src/main/java/PassByValueExample.java
@@ -0,0 +1,21 @@
+public class PassByValueExample {
+
+	void changeString(String s) {
+		// s=null;
+		s = "bbbb";
+	}
+
+	void changeInt(int i) {
+		i = 0;
+	}
+
+	public static void main(String[] args) {
+		String s = "aaaa";
+		int i = 10;
+		System.out.println("1:" + s + ">" + i);
+		new PassByValueExample().changeString(s);
+		new PassByValueExample().changeInt(i);
+		System.out.println("2:" + s + ">" + i);
+	}
+
+}
