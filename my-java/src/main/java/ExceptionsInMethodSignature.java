66ec57d04341505436c66d730b132cbfb4d32e24
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-04-19 11:12:57 +0000

    clean up

--- my-java/src/main/java/ExceptionsInMethodSignature.java
+++ my-java/src/main/java/ExceptionsInMethodSignature.java
@@ -11,7 +11,7 @@ class Dddd extends Ccc {
     }
 }
 
-public class Myexceptions {
+public class ExceptionsInMethodSignature {
 	public static void main(String[] args) throws Exception {
         Ccc c = new Dddd();
 		c.myM();
