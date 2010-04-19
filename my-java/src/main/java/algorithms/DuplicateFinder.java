2d8385c3b4c340c74265aa8bb9c6b932bc9522cf
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-04-19 11:11:57 +0000

    clean up

--- my-java/src/main/java/algorithms/DuplicateFinder.java
+++ my-java/src/main/java/algorithms/DuplicateFinder.java
@@ -1,3 +1,4 @@
+package algorithms;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,9 +8,8 @@ public class DuplicateFinder {
             { "Cat", "Dog", "Tiger", "Lion", "Lion" };
 
     public static void main(String[] args) {
-        Set<String> set = new HashSet<String>();
-        for (int i = 0; i < strArray.length; i++) {
-            String element = strArray[i];
+		Set<String> set = new HashSet<String>();
+    	for (String element : strArray) {
             if (set.contains(element)) {
                 System.out.println("Duplicated element: " + element);
                 break;
