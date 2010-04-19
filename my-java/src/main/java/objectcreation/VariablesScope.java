310862c926ea4c265ba419c8c5c82cd02ad090e6
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-04-19 10:01:20 +0000

    clean up

---
+++ my-java/src/main/java/objectcreation/VariablesScope.java
@@ -0,0 +1,43 @@
+package objectcreation;
+class Abcd {
+    String color;
+    @Override
+    public String toString() {
+        return color;
+    }
+}
+
+public class VariablesScope {
+
+    // can have a value not assigned, but still can be used
+    // system assigns the default values 0/null/false
+    // objects are created on the heap, instance variables (as well)
+    // heap is shared amongst threads
+    int heapMemberVariable;
+
+    int mv = heapMemberVariable;
+
+    private static void myMethod() {
+        // local variable has to be initialized in order to be used
+        // stack (LIFO) - separate for each thread, each thread executing a
+        // method has its own copy of local variables;
+        // -Xss to increase the stack
+        int stackLocalVariable = 0;
+        int lv = stackLocalVariable;
+        System.out.println(lv);
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+        Abcd a = new Abcd();
+        a.color = "red";
+        Abcd b = a;
+        a = null;
+        System.out.println(b);
+        System.out.println(a);
+        
+    }
+
+    // idempotent method - repeated calls with the same args give the same
+    // results
+}
