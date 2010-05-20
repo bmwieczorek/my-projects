90c7002ff538604c30714d20958f54c1b24fd1a1
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-05-20 12:45:46 +0000

    updated for windows

--- build-all.sh
+++ build-all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find -maxdepth 1 -type d -name my-\* -exec bash -c "(cd {} &&  mvn clean install eclipse:eclipse)"  \;
+find -maxdepth 1 -type d -name my-\* -exec bash -c "(cd {} &&  mvn clean install eclipse:eclipse -DdownloadSources=true)"  \;
 
