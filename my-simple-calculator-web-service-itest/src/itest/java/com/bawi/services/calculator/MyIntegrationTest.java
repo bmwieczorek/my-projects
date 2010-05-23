a58beae7838d2933be83247cc249d824ba0b5d19
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-05-23 21:13:45 +0000

    added itest project

---
+++ my-simple-calculator-web-service-itest/src/itest/java/com/bawi/services/calculator/MyIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.bawi.services.calculator;
+
+import org.junit.Test;
+
+
+public class MyIntegrationTest {
+
+	@Test
+	public void testname() throws Exception {
+		System.err.println("aaa");
+	}
+}
