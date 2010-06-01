8375b374043a297f9ddbfefb0af389b84c4ba37a
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-06-01 10:03:00 +0000

    added eclipse clean

--- build-all.sh
+++ build-all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find -maxdepth 1 -type d -name my-\* -exec bash -c "(cd {} &&  mvn clean install eclipse:eclipse -DdownloadSources=true)"  \;
+find -maxdepth 1 -type d -name my-\* -exec bash -c "(cd {} &&  mvn clean install eclipse:clean eclipse:eclipse -DdownloadSources=true)"  \;
 
