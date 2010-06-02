0a2f481a5248a5b05abc25e22de9894321b7249f
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-06-02 21:24:03 +0000

    formatting

--- my-request-forwarder/src/main/java/com/bawi/servlet/RequestForwardingServlet.java
+++ my-request-forwarder/src/main/java/com/bawi/servlet/RequestForwardingServlet.java
@@ -12,25 +12,27 @@ import org.apache.log4j.Logger;
 
 public class RequestForwardingServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     private final static Logger logger = Logger.getLogger(RequestForwardingServlet.class);
     private final static String targetContext = "/service";
 
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         forwardRequestToContext(req, resp, targetContext);
-	}
-	
+    }
+
     private void forwardRequestToContext(HttpServletRequest req, HttpServletResponse resp, String targetContext)
             throws ServletException, IOException {
         ServletContext context = getServletContext().getContext(targetContext);
         validate(context);
+        String originContext = req.getContextPath();
         String requestPathWithoutContext = req.getPathInfo();
+        logger.debug("Forwarding request from " + originContext + requestPathWithoutContext + " to " + targetContext
+                + requestPathWithoutContext);
         context.getRequestDispatcher("/" + requestPathWithoutContext).forward(req, resp);
-        logger.debug("Forwarding request to " + req.getContextPath() + "/" + requestPathWithoutContext);
     }
 
-    private void validate(ServletContext context) {
+    private static void validate(ServletContext context) {
         if (context == null) {
             String message = "No deployable at target context " + targetContext;
             logger.error(message);
