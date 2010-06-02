e14e117fe8e370ac77e43250c2f7d98dd84926b7
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-06-02 21:24:51 +0000

    added

---
+++ my-java/src/main/java/PrintSystemProperties.java
@@ -0,0 +1,11 @@
+import java.util.Properties;
+
+
+public class PrintSystemProperties {
+	public static void main(String[] args) {
+		Properties properties = System.getProperties();
+		for (String property : properties.stringPropertyNames()) {
+			System.out.println(property + "=" + properties.getProperty(property));
+		}
+	}
+}
