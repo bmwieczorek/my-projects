ed45752d59d2832ed18c67331ee441467c10681e
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-06-06 10:02:56 +0000

    svn ignore for org.eclipse.jdt.core.prefs

---
+++ my-maven-plugin/src/main/java/com/bawi/MyMojo.java
@@ -0,0 +1,26 @@
+package com.bawi;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * Says hello
+ * 
+ * @goal tell
+ * @phase process-sources
+ */
+public class MyMojo extends AbstractMojo {
+
+    /**
+     * My parameter
+     * 
+     * @parameter expression=${tell.text}
+     */
+    private String text;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("Hello world " + System.getProperty("text") + text);
+    }
+
+}
