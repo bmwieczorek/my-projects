3da25b384b3d3e36152b8df8ae113765be64ae92
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-06-15 13:53:04 +0000

    updated some versions

--- my-multi-module-maven/my-multi-module-maven-war/src/main/java/com/bawi/servlet/AboutServlet.java
+++ my-multi-module-maven/my-multi-module-maven-war/src/main/java/com/bawi/servlet/AboutServlet.java
@@ -2,6 +2,8 @@ package com.bawi.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Properties;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+
+import com.bawi.WarMainPropertiesFileReader;
 
 public class AboutServlet extends HttpServlet {
 
@@ -19,13 +23,14 @@ public class AboutServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
             IOException {
         PrintWriter writer = resp.getWriter();
-        // Properties properties = PropertiesFileReader.readPropertiesFromResource("my.properties");
-        // Set<String> stringPropertyNames = properties.stringPropertyNames();
-        // for (String propertyName : stringPropertyNames) {
-        // String message = propertyName + "=" + properties.getProperty(propertyName);
-        // writer.println(message);
-        // logger.debug(message);
-        // }
+        Properties properties = WarMainPropertiesFileReader
+                .readPropertiesFromResource("my-war-main.properties");
+        Set<String> stringPropertyNames = properties.stringPropertyNames();
+        for (String propertyName : stringPropertyNames) {
+            String message = propertyName + "=" + properties.getProperty(propertyName);
+            writer.println(message);
+            logger.debug(message);
+        }
         writer.flush();
         writer.close();
     }
