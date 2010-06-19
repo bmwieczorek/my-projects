60a4ad9fab7158e5f6286231600b04d9ebcf2654
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-06-19 09:34:17 +0000

    fix classpath problems with cxf

---
+++ my-simple-calculator-web-service/src/main/java/com/bawi/services/calculator/servlet/CalculatorServlet.java
@@ -0,0 +1,19 @@
+package com.bawi.services.calculator.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CalculatorServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() throws ServletException {
+
+		String[] configLocations = new String[] { "cxf-context.xml",
+				"services-context.xml" };
+		new ClassPathXmlApplicationContext(configLocations);
+	}
+}
