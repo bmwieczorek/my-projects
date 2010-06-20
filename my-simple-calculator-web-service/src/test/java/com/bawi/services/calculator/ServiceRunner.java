46c50b791cb04c51e11f6f7d99dcf2fd45c73878
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-06-20 21:01:12 +0000

    itests

--- my-simple-calculator-web-service/src/test/java/com/bawi/services/calculator/ServiceRunner.java
+++ my-simple-calculator-web-service/src/test/java/com/bawi/services/calculator/ServiceRunner.java
@@ -1,8 +1,11 @@
-package com.bawi.services.calculator.runner;
+package com.bawi.services.calculator;
 
+import org.apache.log4j.Logger;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ServiceRunner {
+
+    private static final Logger logger = Logger.getLogger(ServiceRunner.class);
 
     public ServiceRunner() {
         String[] configLocations = new String[] { "cxf-context.xml", "services-context.xml" };
@@ -10,6 +13,7 @@ public class ServiceRunner {
     }
 
     public static void main(String[] args) {
+        logger.debug("Starting service runner");
         new ServiceRunner();
         while (true) {
             try {
