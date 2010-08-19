9263fc6c70eefb5465c6abb0480cca3347d7317d
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-08-19 14:52:51 +0000

    log4j extras

--- my-log4j-mailing-webapp/src/main/java/com/bawi/service/MyService.java
+++ my-log4j-mailing-webapp/src/main/java/com/bawi/service/MyService.java
@@ -7,8 +7,23 @@ public class MyService {
     private static final Logger logger = Logger.getLogger(MyService.class);
 
     public MyService() {
+        for (int i = 0; i < 500; i++) {
+            error();
+            sleep(500);
+        }
+    }
+
+    private void sleep(int sleepMillis) {
+        try {
+            Thread.sleep(sleepMillis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void error() {
         logger.debug("Hi");
-        throw new RuntimeException("My service exception");
+        // throw new RuntimeException("My service exception");
     }
 
 }
