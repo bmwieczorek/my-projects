512400e4af90e899484be4d11eac854ee213298d
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-09-12 20:53:12 +0000

    removed

--- apache-tomcat-5.5.28/bin/setenv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-
-JAVA_OPTS='-Xmx1024m -XX:MaxPermSize=512m -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8082 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false'
-
-export JAVA_OPTS 
