77fc7b32d8819476ba7d3ba58a68648733317966
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-09-26 22:04:01 +0000

    done

--- environment/tomcat/install-tomcat-with-separate-catalina-base.sh
+++ environment/tomcat/install-tomcat-with-separate-catalina-base.sh
@@ -1,48 +1,57 @@
 #!/bin/bash
 
-#go to directory where you want to install tomcat
-
 TOMCAT_VERSION=6.0.26
-BASE_DIR=$(pwd)
-CURRENT_DIR=${BASE_DIR}
-
-echo "Current directory is ${CURRENT_DIR}"
-
-echo "Downloading tomcat ${TOMCAT_VERSION} ..."
-wget http://archive.apache.org/dist/tomcat/tomcat-6/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz
-
-echo "Expanding tomcat to directory ${CURRENT_DIR}"
-tar xzf apache-tomcat-${TOMCAT_VERSION}.tar.gz
-
-cd ${CURRENT_DIR}/apache-tomcat-${TOMCAT_VERSION}
-
-CURRENT_DIR=$(pwd)
-echo "Current directory is ${CURRENT_DIR}"
-
-# CATALINA_BASE is a directory of one of tomcat instances
-CATALINA_BASE=${BASE_DIR}/DEV-catalina-base
-echo "CATALINA_BASE=${CATALINA_BASE}"
+CATALINA_HOME="$(pwd)"/apache-tomcat-${TOMCAT_VERSION}
+CATALINA_BASE="$(pwd)"/DEV-catalina-base
+CATALINA_PID="${CATALINA_BASE}"/catalina.pid
 
 # CATALINA_HOME is a directory created from expanded apache-tomcat-${TOMCAT_VERSION}.tar.gz, shared across tomcat instances
-CATALINA_HOME=${BASE_DIR}/apache-tomcat-${TOMCAT_VERSION}
 echo "CATALINA_HOME=${CATALINA_HOME}"
 
+# CATALINA_BASE is a directory of one of tomcat instances
+echo "CATALINA_BASE=${CATALINA_BASE}"
+
+echo "CATALINA_PID=${CATALINA_PID}"
+
+#echo "Downloading tomcat ${TOMCAT_VERSION} ..."
+#wget http://archive.apache.org/dist/tomcat/tomcat-6/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz
+
+echo "Expanding tomcat to directory ${CURRENT_DIR}"
+#tar xzf apache-tomcat-${TOMCAT_VERSION}.tar.gz
+unzip -q apache-tomcat-${TOMCAT_VERSION}.zip
+
 echo "Setting up catalina base ... "
-mkdir -p ${CATALINA_BASE}/{bin,conf,logs,temp,webapps,work}
+mkdir -p "${CATALINA_BASE}"/{bin,conf,logs,temp,webapps,work}
 
 # create your own if you want to override some settings (e.g. ports)
-cp ${CATALINA_HOME}/conf/server.xml ${CATALINA_BASE}/conf
-cp ${CATALINA_HOME}/conf/web.xml ${CATALINA_BASE}/conf
+cp "${CATALINA_HOME}"/conf/server.xml "${CATALINA_BASE}"/conf
+cp "${CATALINA_HOME}"/conf/web.xml "${CATALINA_BASE}"/conf
+cp -r "${CATALINA_HOME}"/webapps/ROOT "${CATALINA_BASE}"/webapps
 
-cp ${CATALINA_HOME}/bin/catalina.sh ${CATALINA_BASE}/bin
+# tomcat manager - redirect folder location
+mkdir -p "${CATALINA_BASE}"/conf/Catalina/localhost
+echo '<?xml version="1.0" encoding="UTF-8"?>' >> "${CATALINA_BASE}"/conf/Catalina/localhost/manager.xml
+echo '<Context docBase="${catalina.home}/webapps/manager" privileged="true" antiResourceLocking="false" />' >> "${CATALINA_BASE}"/conf/Catalina/localhost/manager.xml
 
-# specify your env variables in setenv.sh
+# tomcat manager - login credentials
+echo "<?xml version='1.0' encoding='utf-8'?>" >> "${CATALINA_BASE}"/conf/tomcat-users.xml 
+echo '<tomcat-users>' >> "${CATALINA_BASE}"/conf/tomcat-users.xml 
+echo '<role rolename="manager"/>' >> "${CATALINA_BASE}"/conf/tomcat-users.xml 
+echo '<user username="admin" password="" roles="manager"/>' >> "${CATALINA_BASE}"/conf/tomcat-users.xml 
+echo '</tomcat-users>' >> "${CATALINA_BASE}"/conf/tomcat-users.xml 
 
 
-echo '#!/bin/bash' >> ${CATALINA_BASE}/bin/startup.sh
-echo "export CATALINA_BASE=${CATALINA_BASE}" >> ${CATALINA_BASE}/bin/startup.sh
-echo "export CATALINA_HOME=${CATALINA_HOME}" >> ${CATALINA_BASE}/bin/startup.sh
-echo 'exec "$CATALINA_HOME"/bin/startup.sh "$@"' >> ${CATALINA_BASE}/bin/startup.sh
-chmod +x ${CATALINA_BASE}/bin/startup.sh
+echo '#!/bin/bash' >> "${CATALINA_BASE}"/bin/startup.sh
+echo "export CATALINA_BASE=\"${CATALINA_BASE}\"" >> "${CATALINA_BASE}"/bin/startup.sh
+echo "export CATALINA_HOME=\"${CATALINA_HOME}\"" >> "${CATALINA_BASE}"/bin/startup.sh
+echo "export CATALINA_PID=\"${CATALINA_PID}\"" >> "${CATALINA_BASE}"/bin/startup.sh
+echo '"${CATALINA_HOME}"'/bin/startup.sh  >> "${CATALINA_BASE}"/bin/startup.sh
 
+echo '#!/bin/bash' >> "${CATALINA_BASE}"/bin/shutdown.sh
+echo "export CATALINA_BASE=\"${CATALINA_BASE}\"" >> "${CATALINA_BASE}"/bin/shutdown.sh
+echo "export CATALINA_HOME=\"${CATALINA_HOME}\"" >> "${CATALINA_BASE}"/bin/shutdown.sh
+echo "export CATALINA_PID=\"${CATALINA_PID}\"" >> "${CATALINA_BASE}"/bin/shutdown.sh
+echo '"${CATALINA_HOME}"'/bin/shutdown.sh  >> "${CATALINA_BASE}"/bin/shutdown.sh
 
+cd "${CATALINA_HOME}" && find -name \*sh -exec chmod +x {} \; 
+cd "${CATALINA_BASE}" && find -name \*sh -exec chmod +x {} \; 
