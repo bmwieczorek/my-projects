aacb41bdf53b2118d282e8728d05b4becf75d3ed
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2010-09-27 11:40:16 +0000

    done

--- environment/tomcat/DEV-catalina-base-working/bin/startup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export CATALINA_BASE="/home/bawi/workspace/environment/tomcat/DEV-catalina-base"
-export CATALINA_HOME="/home/bawi/workspace/environment/tomcat/apache-tomcat-6.0.26"
-export CATALINA_PID="/home/bawi/workspace/environment/tomcat/DEV-catalina-base/catalina.pid"
-"/home/bawi/workspace/environment/tomcat/apache-tomcat-6.0.26"/bin/startup.sh
