ca0f36fd8dd139dddd6b4d80d928bf0fbdb5d545
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2011-04-03 19:05:55 +0000

    added

---
+++ my-source-checks-test-parent/my-source-checks-test-common/src/test/java/sourcechecks/ClassToTestSourceCheckTest.java
@@ -0,0 +1,23 @@
+package sourcechecks;
+
+/**
+ * 
+ * @author Bartosz Wieczorek
+ * 
+ *         This class is intentionally created to test Findbugs, PMS and
+ *         Checkstyle
+ */
+public class ClassToTestSourceCheckTest {
+
+	// int b() {
+	// if (a > 0)
+	// try {
+	// return 10;
+	// } catch (Exception e) {
+	// }
+	// new RuntimeException();
+	// return 100;
+	// }
+	//
+	// int a;
+}
