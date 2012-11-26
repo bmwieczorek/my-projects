5a203f07da05e49e82e6c1a4f43dd0d0404fda00
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2012-11-26 18:11:55 +0000

    added foreach example

--- my-xslt/src/test/java/com/bawi/xslt/XsltConverterTest.java
+++ my-xslt/src/test/java/com/bawi/xslt/XsltConverterTest.java
@@ -10,14 +10,22 @@ import org.springframework.core.io.Resource;
 
 public class XsltConverterTest {
 
-    private final Resource resource = new ClassPathResource("template1.xsl");
-
     private static final Logger LOGGER = LoggerFactory.getLogger(XsltConverterTest.class);
 
     @Test
     public void shouldConvert() throws Exception {
+        Resource resource = new ClassPathResource("template1.xsl");
         XsltConverter converter = new XsltConverter(resource);
         File file = new ClassPathResource("test1.xml").getFile();
+        String result = converter.convert(file);
+        LOGGER.info("Converted xml:\n{}", result);
+    }
+
+    @Test
+    public void shouldConvert2() throws Exception {
+        Resource resource = new ClassPathResource("template2.xsl");
+        XsltConverter converter = new XsltConverter(resource);
+        File file = new ClassPathResource("test2.xml").getFile();
         String result = converter.convert(file);
         LOGGER.info("Converted xml:\n{}", result);
     }
