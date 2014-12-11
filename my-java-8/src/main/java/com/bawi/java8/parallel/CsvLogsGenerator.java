a9f87c4717a2db0725f9412205cdbadec5b2d392
bmwieczorek <bmwieczorek@fc2e1660-16fd-11df-bf8b-afb152d3ee00>
2014-12-11 10:21:43 +0000

    updated my java 8 to remove unused imports

--- my-java-8/src/main/java/com/bawi/java8/parallel/CsvLogsGenerator.java
+++ my-java-8/src/main/java/com/bawi/java8/parallel/CsvLogsGenerator.java
@@ -11,11 +11,11 @@ public class CsvLogsGenerator {
 
     public static void main(String[] args) throws InterruptedException {
         //for (int j = 0; j < 649; j++) {      // MaxFileSize 10KB  no sleep, creates 7 files up to 100 lines:  feedback: show fork join process - see target (analysis)
-        for (int j = 0; j < 29999999; j++) { // MaxFileSize 500MB no sleep, creates 3GB (6 x 0.5GB):          feedback: can read 3GB despite 2GB memory, duration: 127s 
+        //for (int j = 0; j < 29999999; j++) { // MaxFileSize 500MB no sleep, creates 3GB (6 x 0.5GB):          feedback: can read 3GB despite 2GB memory, duration: 127s 
         //for (int j = 0; j < 9999999; j++) {  // MaxFileSize 500MB no sleep, creates 1GB (2 x 0.5GB),          feedback: duration: 33s
-        //for (int j = 0; j < 250; j++) {        // MaxFileSize 10KB  1s sleep, creates ,                         feedback: show stats 1 tps calculation
+        for (int j = 0; j < 250; j++) {        // MaxFileSize 10KB  1s sleep, creates ,                         feedback: show stats 1 tps calculation
             LOGGER.debug("migration-edifacts-14150248704395444.txt,0,{},2014-08-15,EY,3SL2DF", String.format("607456%07d", duplicateEvenNumber(j)));
-//            TimeUnit.SECONDS.sleep(1); // 1 per second - test tps
+            TimeUnit.SECONDS.sleep(1); // 1 per second - test tps
         }
     }
 
