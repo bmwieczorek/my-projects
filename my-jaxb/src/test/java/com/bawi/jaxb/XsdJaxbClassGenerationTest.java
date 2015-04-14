525d4973d4dfa8e59ff98457e86e00e4384b3464
Bartosz Wieczorek <bmwieczorek@gmail.com>
2015-04-14 10:35:22 +0200

    added 3 scenarios for jaxb

--- my-jaxb/src/test/java/com/bawi/jaxb/XsdJaxbClassGenerationTest.java
+++ my-jaxb/src/test/java/com/bawi/jaxb/XsdJaxbClassGenerationTest.java
@@ -14,10 +14,30 @@ import org.junit.Test;
 public class XsdJaxbClassGenerationTest {
 
     @Test
-    public void shouldMarshallToXml() throws JAXBException {
+    public void shouldMarshallToXmlNormal() throws JAXBException {
+        com.bawi.jaxb.normal.MyServiceRQ myServiceRQ = new com.bawi.jaxb.normal.MyServiceRQ();
+        com.bawi.jaxb.normal.MyElement myElement = new com.bawi.jaxb.normal.MyElement();
+        myElement.setMyAttribute("some attribute value");
+        myServiceRQ.setMyElement(myElement);
+        myServiceRQ.setMyAttribute(Integer.valueOf(123));
+        //@formatter:off
+        String expectedXml = 
+                "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+                "<MyServiceRQ myAttribute=\"123\">\n" +
+                "    <MyElement myAttribute=\"some attribute value\"/>\n" +
+                "</MyServiceRQ>\n";
+        //@formatter:on
+
+        String generatedXml = marshall(myServiceRQ);
+
+        assertEquals(expectedXml, generatedXml); 
+    }
+    
+    @Test
+    public void shouldMarshallToXmlSameAttributeAndElement() throws JAXBException {
         // given
-        MyServiceRQ myServiceRQ = new MyServiceRQ();
-        MyElement myElement = new MyElement();
+        com.bawi.jaxb.same.attribute.and.element.MyServiceRQ myServiceRQ = new com.bawi.jaxb.same.attribute.and.element.MyServiceRQ();
+        com.bawi.jaxb.same.attribute.and.element.MyElement myElement = new com.bawi.jaxb.same.attribute.and.element.MyElement();
         myElement.setMyAttribute("some attribute value");
         myServiceRQ.setMyElement(myElement);
         myServiceRQ.setMyElementAttribute(Integer.valueOf(123));
@@ -36,9 +56,32 @@ public class XsdJaxbClassGenerationTest {
         assertEquals(expectedXml, generatedXml); 
     }
 
-    private String marshall(MyServiceRQ myServiceRQ) throws JAXBException, PropertyException {
-        //JAXBContext jaxbContext = JAXBContext.newInstance(com.bawi.jaxb.MyServiceRQ.class);
-        JAXBContext jaxbContext = JAXBContext.newInstance("com.bawi.jaxb");
+    @Test
+    public void shouldMarshallToXmlSameElementAndComplexType() throws JAXBException {
+        // given
+        com.bawi.jaxb.same.element.and.complextype.MyServiceRQ myServiceRQ = new com.bawi.jaxb.same.element.and.complextype.MyServiceRQ();
+        com.bawi.jaxb.same.element.and.complextype.MyElement myElement = new com.bawi.jaxb.same.element.and.complextype.MyElement();
+        myElement.setMyAttribute("some attribute value");
+        myServiceRQ.setMyElement(myElement);
+        myServiceRQ.setMyAttribute(Integer.valueOf(123));
+        //@formatter:off
+        String expectedXml = 
+                "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+                "<MyServiceRQ myAttribute=\"123\">\n" +
+                "    <MyElement myAttribute=\"some attribute value\"/>\n" +
+                "</MyServiceRQ>\n";
+        //@formatter:on
+
+        // when
+        String generatedXml = marshall(myServiceRQ);
+
+        // then
+        assertEquals(expectedXml, generatedXml); 
+    }
+
+    private String marshall(Object myServiceRQ) throws JAXBException, PropertyException {
+        //JAXBContext jaxbContext = JAXBContext.newInstance("com.bawi.jaxb");
+        JAXBContext jaxbContext = JAXBContext.newInstance(myServiceRQ.getClass());
         Marshaller marshaller = jaxbContext.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
         StringWriter stringWriter = new StringWriter();
