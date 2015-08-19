b3149961e2c12bef6968718d7c32931b608b3a41
Bartosz Wieczorek <bmwieczorek@gmail.com>
2015-08-19 18:12:43 +0200

    addem my-apache-http-client

---
+++ my-apache-http-client/src/test/java/http/client/FluentProxyTest.java
@@ -0,0 +1,23 @@
+package http.client;
+
+import java.io.IOException;
+
+import org.apache.http.HttpHost;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+
+public class FluentProxyTest {
+    public static void main(String[] args) throws ClientProtocolException, IOException {
+        Executor executor = Executor.newInstance()
+                .auth(new HttpHost("tkthli702.sabre.com", 8090), Settings.USERNAME, Settings.PASSWORD);
+
+        
+        String content = executor
+                .execute(Request.Get("http://tkthli702.sabre.com:8090/view?pattern=6074551101538&path=/apps/tkt-hub/logs/t2.emd*.log&hosts=tkthli702"))
+                .returnContent()
+                .asString();
+
+        System.err.println(content);
+    }
+}
---
+++ my-apache-http-client/src/test/java/http/client/ProxyAuthFarelogixTest.java
@@ -0,0 +1,60 @@
+package http.client;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+public class ProxyAuthFarelogixTest {
+
+    public static void main(String[] args) throws IOException {
+        String xmlRq = FileUtils.readFileToString(new File("src/test/resource/EMDImageRQ.xml"));
+        HttpClient client = createHttpClient();
+        HttpPost post = new HttpPost("https://stg.farelogix.com/xmlts/sandboxdm");
+        post.setEntity(new StringEntity(xmlRq));
+        
+        // when
+        HttpResponse response = client.execute(post);
+
+        // then
+        String content = getContent(response);
+        System.err.println(content);
+    }
+
+    private static HttpClient createHttpClient() {
+        HttpHost proxy = new HttpHost(Settings.PROXY_HOST, Settings.PROXY_PORT);
+        Credentials credentials = new UsernamePasswordCredentials(Settings.USERNAME, Settings.PASSWORD);
+        AuthScope authScope = new AuthScope(Settings.PROXY_HOST, Settings.PROXY_PORT);
+        CredentialsProvider credsProvider = new BasicCredentialsProvider();
+        credsProvider.setCredentials(authScope, credentials);
+        HttpClient client = HttpClientBuilder
+                .create()
+                .setProxy(proxy)
+                .setDefaultCredentialsProvider(credsProvider)
+                .build();
+        return client;
+    }
+
+    private static String getContent(HttpResponse response) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            result.append(line);
+        }
+        return result.toString();
+    }
+}
