a8463a913ac918ad7752bfac2c77dacfb5a6b932
Bartosz Wieczorek <bmwieczorek@gmail.com>
2015-10-19 17:51:15 +0200

    my-java-thread# added case when main thread needs to wait for t1 thread

---
+++ my-java-threads/src/main/java/com/bawi/threads/OnThreadWainingOnAnotherByObjectMonitor.java
@@ -0,0 +1,52 @@
+package com.bawi.threads;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OnThreadWainingOnAnotherByObjectMonitor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OnThreadWainingOnAnotherByObjectMonitor.class);
+
+    private static final Object MONITOR = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        LOGGER.debug("Started");
+        Thread thread = new Thread(() -> {
+            LOGGER.debug("Started");
+            sleepMillis(1000); // additional sleel time to show that main thread will also need to wait
+            synchronized (MONITOR) {
+                MONITOR.notify();
+            }
+            LOGGER.debug("Finished");
+        });
+        thread.start();
+        synchronized (MONITOR) {
+            MONITOR.wait();
+        }
+        LOGGER.debug("Finished");
+    }
+
+    public static synchronized void sleep1() {
+        sleepMillis(1000);
+    }
+
+    public static synchronized void sleep2() {
+        sleepMillis(1000);
+    }
+
+    public static void sleepMillis(int millis) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+/*
+2015-10-19 17:49:14,447|main    |Started
+2015-10-19 17:49:14,498|Thread-0|Started
+
+2015-10-19 17:49:15,498|Thread-0|Finished
+2015-10-19 17:49:15,498|main    |Finished
+
+*/
