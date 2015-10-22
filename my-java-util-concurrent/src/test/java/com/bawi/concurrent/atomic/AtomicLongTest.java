051de5ab2d4de2a92de0ee2b86f7b53b3421c4c5
Bartosz Wieczorek <bmwieczorek@gmail.com>
2015-10-22 12:35:12 +0200

    my-java-concurrent#updated AtomicLongTest to use set value

--- my-java-util-concurrent/src/test/java/com/bawi/concurrent/atomic/AtomicLongTest.java
+++ my-java-util-concurrent/src/test/java/com/bawi/concurrent/atomic/AtomicLongTest.java
@@ -15,5 +15,9 @@ public class AtomicLongTest {
         assertEquals(2, new AtomicLong().addAndGet(2L));
         assertEquals(9, new AtomicLong(3L).updateAndGet(n -> n * n));
         assertEquals(6, new AtomicLong(3L).accumulateAndGet(2, (n,x) -> n * x));
+
+        AtomicLong atomicLong = new AtomicLong();
+        atomicLong.set(1);
+        assertEquals(1, atomicLong.get());
     }
 }
