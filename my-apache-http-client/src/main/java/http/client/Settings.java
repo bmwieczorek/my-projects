016404a1414faad0edbc7598d18d66088147a292
Bartosz Wieczorek <bmwieczorek@gmail.com>
2015-11-03 09:00:49 +0100

    my-apache-http-client# moved to src main resources

---
+++ my-apache-http-client/src/main/java/http/client/Settings.java
@@ -0,0 +1,10 @@
+package http.client;
+
+public class Settings {
+
+    public static String USERNAME = "user"; 
+    public static String PASSWORD = "pass"; 
+    public static String PROXY_HOST = "proxy-host"; 
+    public static int PROXY_PORT = 80; 
+}
+
