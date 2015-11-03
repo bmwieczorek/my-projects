ab376604ba13a1c8465ef8d640c4e8bb0f8acb50
Bartosz Wieczorek <bmwieczorek@gmail.com>
2015-11-03 08:24:36 +0100

    my-apache-http-client# added stop watch

---
+++ my-apache-http-client/src/test/java/com/bawi/threads/cpu/monitoring/StopWatch.java
@@ -0,0 +1,46 @@
+package com.bawi.threads.cpu.monitoring;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+import java.util.concurrent.TimeUnit;
+
+public class StopWatch {
+
+    private final ThreadMXBean bean;
+    private long startCpuTime;
+    private long startUserTime;
+    private long startNano;
+    private long stopCpuTime;
+    private long stopUserTime;
+    private long stopNano;
+
+    public StopWatch() {
+        bean = ManagementFactory.getThreadMXBean();
+    }
+
+    public void start() {
+        startCpuTime = bean.getCurrentThreadCpuTime();
+        startUserTime = bean.getCurrentThreadUserTime();
+        startNano = System.nanoTime();
+    }
+
+    public void stop() {
+        stopCpuTime = bean.getCurrentThreadCpuTime();
+        stopUserTime = bean.getCurrentThreadUserTime();
+        stopNano = System.nanoTime();
+    }
+
+    @Override
+    public String toString() {
+        long elapsedNano = stopNano - startNano;
+        long cpuTime = stopCpuTime - startCpuTime;
+        long userTime = stopUserTime - startUserTime;
+        long systemTime = cpuTime - userTime;
+        return 
+            String.format("User (app code only) time:                   %d ms (%,.2f%%)\n", TimeUnit.NANOSECONDS.toMillis(userTime), (double)userTime/elapsedNano * 100) +
+            String.format("System (os code on behalf app e.g.I/O) time: %d ms (%,.2f%%)\n", TimeUnit.NANOSECONDS.toMillis(systemTime), (double)systemTime/elapsedNano * 100) +
+            String.format("CPU (total CPU spent for app) time:          %d ms (%,.2f%%)\n", TimeUnit.NANOSECONDS.toMillis(cpuTime),  (double)cpuTime/elapsedNano * 100) +
+            String.format("Elapsed time:                                %d ms (%,.2f%%)\n", TimeUnit.NANOSECONDS.toMillis(elapsedNano), 100d);
+    }
+
+}
--- my-apache-http-client/src/test/java/http/client/FluentFormLoginAuthorizationTest.java
+++ my-apache-http-client/src/test/java/http/client/FluentFormLoginAuthorizationTest.java
@@ -1,22 +1,45 @@
 package http.client;
 
-import java.io.IOException;
-
 import org.apache.http.HttpHost;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 
+import com.bawi.threads.cpu.monitoring.StopWatch;
+
 public class FluentFormLoginAuthorizationTest {
-    public static void main(String[] args) throws ClientProtocolException, IOException {
+    public static void main(String[] args) throws Exception {
         Executor executor = Executor.newInstance()
                 .auth(new HttpHost("tkthli702.sabre.com", 8090), Settings.USERNAME, Settings.PASSWORD);
 
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
         String content = executor
-                .execute(Request.Get("http://tkthli702.sabre.com:8090/view?pattern=6074551101538&path=/apps/tkt-hub/logs/t2.emd*.log&hosts=tkthli702"))
+                .execute(Request.Get("http://tkthli702.sabre.com:8090/view?pattern=DEBUG&path=/apps/tkt-hub/logs/*&hosts=tkthli702"))
                 .returnContent()
                 .asString();
 
-        System.err.println(content);
+        stopWatch.stop();
+        System.out.println(stopWatch);
+        System.out.println(content);
     }
 }
+
+/*
+User (app code only) time:                   78 ms (0.85%)
+System (os code on behalf app e.g.I/O) time: 46 ms (0.51%)
+CPU (total CPU spent for app) time:          124 ms (1.37%)
+Elapsed time:                                9127 ms (100.00%)
+
+"main" #1 prio=5 os_prio=0 tid=0x00000000021fe000 nid=0x2214 runnable [0x000000000260e000]
+   java.lang.Thread.State: RUNNABLE
+    at java.net.SocketInputStream.socketRead0(Native Method)
+    at java.net.SocketInputStream.socketRead(SocketInputStream.java:116)
+    at java.net.SocketInputStream.read(SocketInputStream.java:170)
+    at java.net.SocketInputStream.read(SocketInputStream.java:141)
+    at org.apache.http.impl.io.SessionInputBufferImpl.streamRead(SessionInputBufferImpl.java:139)
+    at org.apache.http.impl.io.SessionInputBufferImpl.fillBuffer(SessionInputBufferImpl.java:155)
+    at org.apache.http.impl.io.SessionInputBufferImpl.readLine(SessionInputBufferImpl.java:284)
+
+...
+*/
