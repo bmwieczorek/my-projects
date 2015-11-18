d7468c73493a7851bb1a045e60307472579da464
Bartosz Wieczorek <bmwieczorek@gmail.com>
2015-11-18 19:14:34 +0100

    my-java-agent#updated to add change from Foo to Bar

--- my-java-agent/src/main/java/com/bawi/agent/MyJavaAgent.java
+++ my-java-agent/src/main/java/com/bawi/agent/MyJavaAgent.java
@@ -15,17 +15,16 @@ public class MyJavaAgent {
         inst.addTransformer(new ClassFileTransformer() {
             @Override
             public byte[] transform(ClassLoader classLoader, String s, Class<?> aClass, ProtectionDomain protectionDomain, byte[] bytes) throws IllegalClassFormatException {
-                System.out.println("In transform for class:" + s );
                 if ("com/bawi/application/MyApplication".equals(s)) {
-                    // Javassist
                     try {
                         ClassPool cp = ClassPool.getDefault();
                         CtClass cc = cp.get("com.bawi.application.MyApplication");
                         CtMethod m = cc.getDeclaredMethod("getHelloMessage");
                         m.addLocalVariable("elapsedTime", CtClass.longType);
-                        m.insertBefore("elapsedTime = System.currentTimeMillis();");
+                        m.insertBefore("name=\"Bar\"; " +
+                                "elapsedTime = System.currentTimeMillis();");
                         m.insertAfter("{elapsedTime = System.currentTimeMillis() - elapsedTime;"
-                                + "System.out.println(\"Method Executed in ms: \" + elapsedTime);}");
+                                + "System.out.println(\"[MyJavaAgent] Method Executed in ms: \" + elapsedTime);}");
                         byte[] byteCode = cc.toBytecode();
                         cc.detach();
                         return byteCode;
--- my-java-agent/src/main/java/com/bawi/application/MyApplication.java
+++ my-java-agent/src/main/java/com/bawi/application/MyApplication.java
@@ -2,10 +2,10 @@ package com.bawi.application;
 
 public class MyApplication {
     public static void main(String[] args) {
-        new MyApplication().getHelloMessage();
+        new MyApplication().getHelloMessage("Foo");
     }
 
-    public void getHelloMessage() {
-        System.out.println("Hello ");
+    public void getHelloMessage(String name) {
+        System.out.println("Hello " + name);
     }
 }
