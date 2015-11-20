fa1be00ac6d62008567daf0268b8cf378740d915
Bartosz Wieczorek <bmwieczorek@gmail.com>
2015-11-20 10:22:04 +0100

    my-java-util-concurrent#updated cyclic barrier

--- my-java-util-concurrent/src/test/java/com/bawi/cyclicbarrier/MyCyclicBarrier.java
+++ my-java-util-concurrent/src/test/java/com/bawi/cyclicbarrier/MyCyclicBarrier.java
@@ -3,40 +3,59 @@ package com.bawi.cyclicbarrier;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.TimeUnit;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class MyCyclicBarrier {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MyCyclicBarrier.class);
-
     public static void main(String[] args) throws InterruptedException {
-        TimeUnit.SECONDS.sleep(10);
+        System.out.println(currentThreadId() + Thread.currentThread().getName() + "|Started");
         CyclicBarrier cyclicBarrier = new CyclicBarrier(3);
-        createAndStartNewThread(cyclicBarrier);
+
+        TimeUnit.SECONDS.sleep(10);
+
+        Thread t0 = createNewThread(cyclicBarrier);
+        t0.start();
+
         TimeUnit.SECONDS.sleep(2);
-        createAndStartNewThread(cyclicBarrier);
+
+        Thread t1 = createNewThread(cyclicBarrier);
+        t1.start();
+
         TimeUnit.SECONDS.sleep(1);
-        createAndStartNewThread(cyclicBarrier);
+
+        Thread t2 = createNewThread(cyclicBarrier);
+        t2.start();
+
+        t0.join();
+        t1.join();
+        t2.join();
+
+        System.out.println(currentThreadId() + Thread.currentThread().getName() + "|Stopped");
     }
 
-    private static void createAndStartNewThread(CyclicBarrier cyclicBarrier) {
+    private static Thread createNewThread(CyclicBarrier cyclicBarrier) {
         Thread thread = new Thread(() -> {
             try {
                 cyclicBarrier.await();
-                LOGGER.debug("Started");
-                TimeUnit.SECONDS.sleep(5);
+                System.out.println(currentThreadId() + Thread.currentThread().getName() + "|Started");
+                for (long l = 0L; l < 9999999999999L; l++) {
+                }
+                System.out.println(currentThreadId() + Thread.currentThread().getName() + "|Stopped");
             } catch (Exception e) {
                 e.printStackTrace();
             }
         });
-        thread.start();
+        return thread;
+    }
+
+    private static String currentThreadId() {
+        long id = Thread.currentThread().getId();
+        return String.format("Thread id=%-2s|", id);
     }
 }
+
 /*
 2015-11-03 08:16:00,562|Thread-2  |Started
 2015-11-03 08:16:00,563|Thread-1  |Started
 2015-11-03 08:16:00,563|Thread-0  |Started
 
-All print threads started at the same time even if the threads were started at different moments
+All print threads started printing at the same time even if the threads were started at different moments
 */
