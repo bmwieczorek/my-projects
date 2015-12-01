a379f815c87c0df87117992cb6705824809f8f03
Bartosz Wieczorek <bmwieczorek@gmail.com>
2015-12-01 11:55:05 +0100

    my-camel-error-handling# added redeliveryDelay

--- my-camel-error-handling/src/main/java/com/bawi/camel/redelivery/MyRouteBuilder.java
+++ my-camel-error-handling/src/main/java/com/bawi/camel/redelivery/MyRouteBuilder.java
@@ -1,8 +1,6 @@
 package com.bawi.camel.redelivery;
 
-import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,37 +20,34 @@ public class MyRouteBuilder extends RouteBuilder {
 
         onException(Throwable.class)
             .handled(true)
-            .log(LoggingLevel.WARN, "com.bawi.camel.redelivery.MyRouteBuilder","in on GLOBAL ON EXCEPTION Throwable")
+            .log(LoggingLevel.WARN, MyRouteBuilder.class.getName(), "in on GLOBAL ON EXCEPTION Throwable")
             .setBody(constant(GLOBAL_ERROR_HANDLER_ERROR_MESSAGE));
 
 
         from(MY_DIRECT_START)
             .onException(IllegalArgumentException.class)
                 .maximumRedeliveries(2)
+                .redeliveryDelay(100)
                 .onRedelivery(myOnRedeliveryProcessor)
                 .handled(true)
-                .log(LoggingLevel.INFO, "com.bawi.camel.redelivery.MyRouteBuilder","in main route ON EXCEPTION IllegalArgumentException")
-                .process(new Processor() {
-                    
-                    @Override
-                    public void process(Exchange exchange) throws Exception {
-                        String body = exchange.getIn().getBody(String.class);
-                        LOGGER.info("body={}", body);
-                        if (body.contains("error")) {
-                            throw new IllegalArgumentException("Max retry limit exceeded");
-                        }
+                .log(LoggingLevel.INFO,  MyRouteBuilder.class.getName(), "in main route ON EXCEPTION IllegalArgumentException")
+                .process(exchange -> {
+                    String body = exchange.getIn().getBody(String.class);
+                    LOGGER.info("body={}", body);
+                    if (body.contains("error")) {
+                        throw new IllegalArgumentException("Max retry limit exceeded");
                     }
                 })
             .end()
-            .log(LoggingLevel.INFO, "com.bawi.camel.redelivery.MyRouteBuilder","in main route before subroute")
+            .log(LoggingLevel.INFO, MyRouteBuilder.class.getName(), "in main route before subroute")
             .to("direct:subroute")
-            .log(LoggingLevel.INFO, "com.bawi.camel.redelivery.MyRouteBuilder","in main - end of processing");
+            .log(LoggingLevel.INFO, MyRouteBuilder.class.getName(), "in main - end of processing");
 
         from("direct:subroute")
             .errorHandler(noErrorHandler()) // disable error handler, so the entire route can be retried in case of redelivery
-            .log(LoggingLevel.INFO, "com.bawi.camel.redelivery.MyRouteBuilder","in subroute before calling MySubSystemProcessor, body=${body}")
+            .log(LoggingLevel.INFO, MyRouteBuilder.class.getName(), "in subroute before calling MySubSystemProcessor, body=${body}")
             .to("bean:mySubSystemProcessor")
-            .log(LoggingLevel.INFO, "com.bawi.camel.redelivery.MyRouteBuilder","in subroute - end of processing");
+            .log(LoggingLevel.INFO, MyRouteBuilder.class.getName(), "in subroute - end of processing");
     }
 
     public void setMyOnRedeliveryProcessor(MyOnRedeliveryProcessor myOnRedeliveryProcessor) {
