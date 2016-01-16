3dabfe8518d404b3c331c6417d706fb0edaca7fe
Bartosz Wieczorek <bmwieczorek@gmail.com>
2016-01-16 23:46:03 +0100

    my-eclipse-groovy-spock

---
+++ my-eclipse-groovy-spock/src/com/bawi/groovy/MyGroovySpockTest.groovy
@@ -0,0 +1,17 @@
+package com.bawi.groovy
+import static org.junit.Assert.*
+import spock.lang.Specification
+
+class MyGroovySpockTest extends Specification { // eclipse run as JUnit test
+
+        def "adding an element leads to size increase"() {  
+        setup: "a new stack instance is created"        
+            def stack = new Stack()
+
+        when:                                           
+            stack.push 42
+
+        then:                                           
+            stack.size() == 1
+    }
+}
---
+++ my-eclipse-groovy-spock/src/com/bawi/groovy/MyGroovyTest.groovy
@@ -0,0 +1,23 @@
+package com.bawi.groovy
+import static groovy.test.GroovyAssert.shouldFail
+
+import org.junit.Test
+
+
+class MyGroovyTest { // run a JUnit test
+
+    @Test
+    void indexOutOfBoundsAccess() {
+        def numbers = [1,2,3,4]
+        shouldFail {
+            numbers.get(4)
+        }
+    }
+    
+    @Test
+    void testSum() {
+        def a = 2, b = 2
+        assert a == b
+    }
+
+}
