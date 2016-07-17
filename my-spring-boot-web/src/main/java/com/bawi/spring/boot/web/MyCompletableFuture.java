4974ffb6c1aec33e510bda4478045b108ca3ff5a
Bartosz Wieczorek <bartosz.wieczorek@sabre.com>
2016-07-17 21:46:52 +0200

    added another my completable future eg

---
+++ my-spring-boot-web/src/main/java/com/bawi/spring/boot/web/MyCompletableFuture.java
@@ -0,0 +1,61 @@
+package com.bawi.spring.boot.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+
+public class MyCompletableFuture {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyCompletableFuture.class);
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + ": started");
+        Arrays.asList("a", "ab", "abc", "abcd")
+            .stream()
+            .forEach(text -> {
+                CompletableFuture<String> stringCF = CompletableFuture.supplyAsync(() -> produceText(text));
+                CompletableFuture<Integer> integerCF = stringCF.thenApplyAsync(MyCompletableFuture::calcStringLength);
+                CompletableFuture<Double> doubleCF = integerCF.thenApply(MyCompletableFuture::calcCirclePerimeter);
+                CompletableFuture<Void> voidCF = doubleCF.thenAccept(MyCompletableFuture::print);
+                CompletableFuture<Void> voidCF2 = voidCF.thenRun(() -> System.out.println("DONE"));
+                }
+            );
+        System.out.println(Thread.currentThread().getName() + ": created completable future, about to sleep");
+        Thread.sleep(5000);
+        System.out.println(Thread.currentThread().getName() + ": finished");
+   }
+
+    static String produceText(String text)  {
+        LOGGER.info("[daemon={}] 1: Producing text: {}", Thread.currentThread().isDaemon(), text);
+        sleepMillis(1000);
+        return text;
+    }
+
+    static int calcStringLength(String text) {
+        int length = text.length();
+        LOGGER.info("[daemon={}] 2: Calculating string length: {}", Thread.currentThread().isDaemon(), length);
+        sleepMillis(1000);
+        return length;
+    }
+
+    static double calcCirclePerimeter(int r) {
+        double perimeter = 2 * Math.PI * r;
+        LOGGER.info("[daemon={}] 3: Calculating circle perimeter: {}", Thread.currentThread().isDaemon(), perimeter);
+        sleepMillis(1000);
+        return perimeter;
+    }
+
+    static void print(Double d) {
+        LOGGER.info("[daemon={}] 4: Printing: {}", Thread.currentThread().isDaemon(), d);
+        System.out.println("d=" + d);
+    }
+
+    private static void sleepMillis(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
