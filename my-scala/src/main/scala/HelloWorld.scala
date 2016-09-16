eb68860ac3f56c06150488e1ee0a6209d89e5af4
Bartosz Wieczorek <bartosz.wieczorek@sabre.com>
2016-09-16 10:02:56 +0200

    updated scala sample

--- my-scala/src/main/scala/HelloWorld.scala
+++ my-scala/src/main/scala/HelloWorld.scala
@@ -1,5 +1,8 @@
 object HelloWorld {
     def main(args: Array[String]): Unit = {
         println("Hello, world!")
+        val ints = Array(1, 2 ,3)
+        val reduce = ints.reduce(_ + _)
+        println(reduce)
     }
 }
