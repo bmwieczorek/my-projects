f23706638b956541249d1f752c98f1e576cb5c0b
Bartosz Wieczorek <bmwieczorek@gmail.com>
2016-09-20 11:42:58 +0200

    added replace with params

--- my-install/replace-placeholders.sh
+++ my-install/replace-placeholders.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-grep -rl mycompany * | xargs sed -i 's/mycompany/YOUR_COMPANY/g'
-grep -rl myusername * | xargs sed -i 's/myusername/YOUR_USERNAME/g'
+grep -rl mycompany * | grep -v replace | xargs sed -i "s/mycompany/$1/g"
+grep -rl myusername * | grep -v replace |  xargs sed -i "s/myusername/$2/g"
 
