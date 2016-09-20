1418cf552ea296a406c619adf5fb029aa61f0361
Bartosz Wieczorek <bmwieczorek@gmail.com>
2016-09-20 18:40:17 +0200

    updated to use my-projects

--- my-install/cloudera/no-proxy.sh
+++ my-install/cloudera/no-proxy.sh
@@ -5,6 +5,6 @@ HOME_DIR=/home/cloudera
 sudo cp $HOME_DIR/dev/env/cloudera/environment.orig /etc/environment
 sudo cp $HOME_DIR/dev/env/cloudera/resolv.conf.orig /etc/resolv.conf
 prefsjs=$(find $HOME_DIR/.mozilla/firefox/ -name prefs.js)
-cp $HOME_DIRdev/env/cloudera/prefs.js.orig $prefsjs
+cp $HOME_DIR/dev/env/cloudera/prefs.js.orig $prefsjs
 cp $HOME_DIR/dev/env/common/.gitconfig.orig $HOME_DIR/.gitconfig
 sudo service cntlmd stop
