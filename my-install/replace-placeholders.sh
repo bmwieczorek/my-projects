54ef3d69c48a416c6850c52d51574e3b2be38466
Bartosz Wieczorek <bmwieczorek@gmail.com>
2016-10-31 16:40:37 +0100

    updated my install to include mydomain

--- my-install/replace-placeholders.sh
+++ my-install/replace-placeholders.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+if ! [ $# -eq 3 ]; then
+    echo "$0: Usage $0 <company> <username> <domain>"
+    exit 1
+fi
 
 grep -rl mycompany * | grep -v replace | xargs sed -i "s/mycompany/$1/g"
 grep -rl myusername * | grep -v replace |  xargs sed -i "s/myusername/$2/g"
+grep -rl mydomain * | grep -v replace |  xargs sed -i "s/mydomain/$3/g"
 
