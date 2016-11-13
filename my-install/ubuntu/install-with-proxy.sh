a9491b71ea651327fdec24621d93aa33f8a076fe
Bartosz Wieczorek <bmwieczorek@gmail.com>
2016-11-13 20:58:02 +0100

    added docker to ubuntu install

--- my-install/ubuntu/install-with-proxy.sh
+++ my-install/ubuntu/install-with-proxy.sh
@@ -101,3 +101,19 @@ $ideaIU/bin/idea.sh
 
 #Install chrome - Import all bookmarks from Chrome: Ctrl+Shift+O
 #start intellij -> create desktop entry and go to centos Applications/Programming and right click Add this launcher to desktop
+
+
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+#echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+apt-cache policy docker-engine
+sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual -y
+sudo apt-get update
+sudo apt-get install docker-engine -y
+sudo service docker start
+sudo groupadd docker
+sudo usermod -aG docker $USER
+#docker run hello-world
+
