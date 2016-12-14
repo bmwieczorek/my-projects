58031c2f3d78999db880207b6c1d4a5ef36b004b
Bartosz Wieczorek <bmwieczorek@gmail.com>
2016-12-14 11:26:55 +0100

    added docker start comments

--- my-install/ubuntu/docker-start-cloudera.sh
+++ my-install/ubuntu/docker-start-cloudera.sh
@@ -1,3 +1,6 @@
 #/bin/bash
-docker run --hostname=quickstart.cloudera --privileged=true -t -i --hostname=quickstart.cloudera --privileged=true -t -i -p 8888:8888 -p80:80  -d $(docker images | grep cloudera | awk '{print $3}') /usr/bin/docker-quickstart
+#run a command in a new container
+#docker run --hostname=quickstart.cloudera --privileged=true -t -i --hostname=quickstart.cloudera --privileged=true -t -i -p 8888:8888 -p80:80  -d $(docker images | grep cloudera | awk '{print $3}') /usr/bin/docker-quickstart
 
+#start one or more stopped containers
+docker run cloudera
