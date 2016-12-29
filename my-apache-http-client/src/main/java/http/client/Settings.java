14be9b05b7b971b6dd4ef0b582a0e7028816fd75
Bartosz Wieczorek <bmwieczorek@gmail.com>
2016-12-29 13:03:57 +0100

    fix

--- my-apache-http-client/src/main/java/http/client/Settings.java
+++ my-apache-http-client/src/main/java/http/client/Settings.java
@@ -2,9 +2,9 @@ package http.client;
 
 public class Settings {
 
-    public static String USERNAME = "sg0212148"; 
-    public static String PASSWORD = "Telekomd1!"; 
-    public static String PROXY_HOST = "www-ad-proxy.sabre.com"; 
+    public static String USERNAME = "myuser"; 
+    public static String PASSWORD = "mypass"; 
+    public static String PROXY_HOST = "myproxy.mycompany.com"; 
     public static int PROXY_PORT = 80; 
 }
 
--- my-install/replace-placeholders.sh
+++ my-install/replace-placeholders.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-if ! [ $# -eq 3 ]; then
-    echo "$0: Usage $0 <company> <username> <domain>"
+if ! [ $# -eq 5 ]; then
+    echo "$0: Usage $0 <company> <username> <domain> <mypassnt> <mypasslm>"
     exit 1
 fi
 
 grep -rl mycompany * | grep -v replace | xargs sed -i "s/mycompany/$1/g"
 grep -rl myuser * | grep -v replace |  xargs sed -i "s/myuser/$2/g"
 grep -rl mydomain * | grep -v replace |  xargs sed -i "s/mydomain/$3/g"
+grep -rl mypassnt * | grep -v replace |  xargs sed -i "s/mypassnt/$4/g"
+grep -rl mypasslm * | grep -v replace |  xargs sed -i "s/mypasslm/$5/g"
 
