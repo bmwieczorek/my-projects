03b67db26145bccfb1ea9175e339ea3e1496bba5
Bartosz Wieczorek <bmwieczorek@gmail.com>
2016-12-29 13:18:38 +0100

    fix

--- my-install/replace-placeholders.sh
+++ my-install/replace-placeholders.sh
@@ -4,9 +4,9 @@ if ! [ $# -eq 5 ]; then
     exit 1
 fi
 
-grep -rl mycompany * | grep -v replace | xargs sed -i "s/mycompany/$1/g"
-grep -rl myuser * | grep -v replace |  xargs sed -i "s/myuser/$2/g"
-grep -rl mydomain * | grep -v replace |  xargs sed -i "s/mydomain/$3/g"
-grep -rl mypassnt * | grep -v replace |  xargs sed -i "s/mypassnt/$4/g"
-grep -rl mypasslm * | grep -v replace |  xargs sed -i "s/mypasslm/$5/g"
+grep -rl mycompany * | grep -v replace | xargs sed -i "" "s/mycompany/$1/g"
+grep -rl myuser *    | grep -v replace | xargs sed -i "" "s/myuser/$2/g"
+grep -rl mydomain *  | grep -v replace | xargs sed -i "" "s/mydomain/$3/g"
+grep -rl mypassnt *  | grep -v replace | xargs sed -i "" "s/mypassnt/$4/g"
+grep -rl mypasslm *  | grep -v replace | xargs sed -i "" "s/mypasslm/$5/g"
 
