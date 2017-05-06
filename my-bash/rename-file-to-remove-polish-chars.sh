62be033d27c3471f647d618398251fee36e551df
Bartosz Wieczorek <bmwieczorek@gmail.com>
2017-05-06 18:50:21 +0200

    added file rename script

--- my-bash/rename-file-to-remove-polish-chars.sh
+++ my-bash/rename-file-to-remove-polish-chars.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
 originalFileName=${1}
-newFileName=$(./remove-polish-chars.sh "${originalFileName}")
+newFileName=$(echo ${1} |\
+    sed 's/Ą/A/g' |\
+    sed 's/ą/a/g' |\
+    sed 's/Ć/C/g' |\
+    sed 's/ć/c/g' |\
+    sed 's/ę/e/g' |\
+    sed 's/Ł/L/g' |\
+    sed 's/ł/l/g' |\
+    sed 's/ń/n/g' |\
+    sed 's/ó/o/g' |\
+    sed 's/Ś/S/g' |\
+    sed 's/ś/s/g' |\
+    sed 's/Ż/Z/g' |\
+    sed 's/ż/z/g' |\
+    sed 's/Ź/Z/g' |\
+    sed 's/ź/z/g' |\
+    sed 's/ą/a/g' |\
+    sed 's/ć/c/g' |\
+    sed 's/ę/e/g' |\
+    sed 's/ł/l/g' |\
+    sed 's/ł/l/g' |\
+    sed 's/ń/n/g' |\
+    sed 's/ó/o/g' |\
+    sed 's/ś/s/g' |\
+    sed 's/Ż/Z/g' |\
+    sed 's/ż/z/g' )
 
 if ! [ "${originalFileName}" == "${newFileName}" ]; then
-    echo "Removed file \"${originalFileName}\" to \"${newFileName}\""
+    echo "Renamed file \"${originalFileName}\" to \"${newFileName}\""
     mv "${originalFileName}" "${newFileName}"
 fi
 
