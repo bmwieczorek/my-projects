a3a833ae64ed9d541b2ad67464c924187410df7d
Bartosz Wieczorek <bmwieczorek@gmail.com>
2017-07-04 11:40:45 +0200

    added fixes

--- my-install/ubuntu/setenv.sh
+++ my-install/ubuntu/setenv.sh
@@ -2,5 +2,6 @@ export MAVEN_OPTS=" -Xms512m -Xmx2048m"
 export JAVA_HOME=~/dev/env/jdk1.8
 export M2_HOME=~/dev/env/apache-maven
 export SCALA_HOME=~/dev/env/scala
-export PATH=$JAVA_HOME/bin:$M2_HOME/bin:$SCALA_HOME/bin:$PATH
-
+export SBT_HOME=~/dev/env/sbt
+export SPARK_HOME=~/dev/env/spark-1.6.3-bin-hadoop2.6
+export PATH=$JAVA_HOME/bin:$M2_HOME/bin:$SCALA_HOME/bin:$SPARK_HOME/bin:$SBT_HOME/bin:$PATH
